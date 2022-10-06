a31c0fba52e7fa61638de31db7b8a08fb95a9162
HyperNotAryanX97 <ahirearyan2016@gmail.com>
2022-10-06 09:39:54 +0000

    Add extras for derp_chime-userdebug 13 TP1A.220905.004 eng.privat.20221002.033711 release-keys
    Signed-off-by: HyperNotAryanX97 <ahirearyan2016@gmail.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## derp_chime-userdebug 13 TP1A.220905.004 eng.privat.20221002.033711 release-keys
+- Manufacturer: xiaomi
+- Platform: bengal
+- Codename: chime
+- Brand: Xiaomi
+- Flavor: derp_chime-userdebug
+- Release Version: 13
+- Id: TP1A.220905.004
+- Incremental: eng.privat.20221002.033711
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: en-US
+- Screen Density: undefined
+- Fingerprint: 
+- OTA version: 
+- Branch: derp_chime-userdebug-13-TP1A.220905.004-eng.privat.20221002.033711-release-keys
+- Repo: xiaomi_chime_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat recovery.img.* 2>/dev/null >> recovery.img
+rm -f recovery.img.* 2>/dev/null
+cat bootimg/01_dtbdump_Qualcomm_Technologies,_Inc._Bengal_SoC.dtb.* 2>/dev/null >> bootimg/01_dtbdump_Qualcomm_Technologies,_Inc._Bengal_SoC.dtb
+rm -f bootimg/01_dtbdump_Qualcomm_Technologies,_Inc._Bengal_SoC.dtb.* 2>/dev/null
+cat product/priv-app/PrebuiltBugle/PrebuiltBugle.apk.* 2>/dev/null >> product/priv-app/PrebuiltBugle/PrebuiltBugle.apk
+rm -f product/priv-app/PrebuiltBugle/PrebuiltBugle.apk.* 2>/dev/null
+cat product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> product/priv-app/Velvet/Velvet.apk
+rm -f product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat product/priv-app/PrebuiltGmsCoreSc/PrebuiltGmsCoreSc.apk.* 2>/dev/null >> product/priv-app/PrebuiltGmsCoreSc/PrebuiltGmsCoreSc.apk
+rm -f product/priv-app/PrebuiltGmsCoreSc/PrebuiltGmsCoreSc.apk.* 2>/dev/null
+cat product/priv-app/DevicePersonalizationPrebuiltPixel2021/DevicePersonalizationPrebuiltPixel2021.apk.* 2>/dev/null >> product/priv-app/DevicePersonalizationPrebuiltPixel2021/DevicePersonalizationPrebuiltPixel2021.apk
+rm -f product/priv-app/DevicePersonalizationPrebuiltPixel2021/DevicePersonalizationPrebuiltPixel2021.apk.* 2>/dev/null
+cat product/app/Maps/Maps.apk.* 2>/dev/null >> product/app/Maps/Maps.apk
+rm -f product/app/Maps/Maps.apk.* 2>/dev/null
+cat product/app/GoogleTTS/GoogleTTS.apk.* 2>/dev/null >> product/app/GoogleTTS/GoogleTTS.apk
+rm -f product/app/GoogleTTS/GoogleTTS.apk.* 2>/dev/null
+cat product/app/LatinIMEGooglePrebuilt/LatinIMEGooglePrebuilt.apk.* 2>/dev/null >> product/app/LatinIMEGooglePrebuilt/LatinIMEGooglePrebuilt.apk
+rm -f product/app/LatinIMEGooglePrebuilt/LatinIMEGooglePrebuilt.apk.* 2>/dev/null
+cat product/app/Photos/Photos.apk.* 2>/dev/null >> product/app/Photos/Photos.apk
+rm -f product/app/Photos/Photos.apk.* 2>/dev/null
+cat product/app/YouTube/YouTube.apk.* 2>/dev/null >> product/app/YouTube/YouTube.apk
+rm -f product/app/YouTube/YouTube.apk.* 2>/dev/null
+cat product/app/PrebuiltGmail/PrebuiltGmail.apk.* 2>/dev/null >> product/app/PrebuiltGmail/PrebuiltGmail.apk
+rm -f product/app/PrebuiltGmail/PrebuiltGmail.apk.* 2>/dev/null
+cat system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system_ext/priv-app/Settings/Settings.apk
+rm -f system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
